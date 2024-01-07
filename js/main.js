08d485898b80583bb4410a51631bc2651238ab62
FaiqTech <hesenovfaiq@icloud.com>
2024-01-08 02:02:58 +0400

    .

--- js/main.js
+++ js/main.js
@@ -1,39 +1,29 @@
-//*Budur bir təqdim düyməsinə sahib bir nümunə html faylı. İndi javascript kodu vasitəsilə paraqraf mətninin tərzini dəyişdirin.
+//*Budur bir təqdim düyməsinə s bir nümunə html faylı. İndi javascript kodu vasitəsilə paraqraf mətninin tərzini dəyişdirin.
 
-// var text = document.getElementById("Text");
-// function Style() {
-//   text.style.fontSize = "25px";
-//   text.style.color = "#7F7FFF";
-// }
-
+function changeStyle() {
+  var text = document.getElementById("Text");
+  text.style.fontSize = "25px";
+  text.style.color = "#7F7FFF";
+}
 //* 3. Paraqraf fon rəngini təyin etmək üçün JavaScript proqramı yazın.
 
-// function changeBackgroundColor() {
-//   // Üçüncü paragrafın ID-sinə əsasən elementi əldə et
-//   var para3 = document.getElementById("para3");
-
-//   // Paragrafın fon rəngini dəyişdir
-//   para3.style.backgroundColor = "lightblue";
-// }
-
+function changeBackgroundColor() {
+  var para3 = document.getElementById("para3");
+  para3.style.backgroundColor = "lightblue";
+}
 //* 4. Budur bir göndərmə düyməsinə sahib bir nümunə HTML faylı. Göstərilən linkin href dəyərini əldə etmək üçün JavaScript funksiyası yazın.
 
-// function getAttributes() {
-//   var a = document.getElementById("coders").href;
-//   alert("linkin href atributunun dəyəri : " + a);
-// }
+function getAttributes() {
+  var a = document.getElementById("coders").href;
+  alert("Linkin href atributunun dəyəri: " + a);
+}
+//* 12. paraqrafın qalın sözlərini, müəyyən bir keçid üzərində siçan üzərində vurğulamaq üçün JavaScript proqramı yazın.
+function highlightBoldWords() {
+  var text = document.getElementById("Text2");
+  var boldWords = text.getElementsByTagName("strong");
+  var highlightClass = "highlight";
 
-//*  paraqrafın qalın sözlərini, müəyyən bir keçid üzərində siçan üzərində vurğulamaq üçün JavaScript proqramı yazın.
-
-// function over() {
-//   var text = document.getElementById("Text");
-
-//   var boldWords = text.getElementsByTagName("strong");
-
-//   var secilmis = "highlight";
-
-//   // Hər bir strong elementin üzərinə vurğulama üçün stil tətbiq et
-//   for (var i = 0; i < boldWords.length; i++) {
-//     boldWords[i].classList.add(secilmis);
-//   }
-// }
+  for (var i = 0; i < boldWords.length; i++) {
+    boldWords[i].classList.add(highlightClass);
+  }
+}
