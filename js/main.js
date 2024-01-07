080aa0d2d8f1b147efd4ca0ce251f071db9dd347
FaiqTech <hesenovfaiq@icloud.com>
2024-01-08 01:48:28 +0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+//*Budur bir təqdim düyməsinə sahib bir nümunə html faylı. İndi javascript kodu vasitəsilə paraqraf mətninin tərzini dəyişdirin.
+
+// var text = document.getElementById("Text");
+// function Style() {
+//   text.style.fontSize = "25px";
+//   text.style.color = "#7F7FFF";
+// }
+
+//* 3. Paraqraf fon rəngini təyin etmək üçün JavaScript proqramı yazın.
+
+// function changeBackgroundColor() {
+//   // Üçüncü paragrafın ID-sinə əsasən elementi əldə et
+//   var para3 = document.getElementById("para3");
+
+//   // Paragrafın fon rəngini dəyişdir
+//   para3.style.backgroundColor = "lightblue";
+// }
+
+//* 4. Budur bir göndərmə düyməsinə sahib bir nümunə HTML faylı. Göstərilən linkin href dəyərini əldə etmək üçün JavaScript funksiyası yazın.
+
+// function getAttributes() {
+//   var a = document.getElementById("coders").href;
+//   alert("linkin href atributunun dəyəri : " + a);
+// }
+
+//*  paraqrafın qalın sözlərini, müəyyən bir keçid üzərində siçan üzərində vurğulamaq üçün JavaScript proqramı yazın.
+
+// function over() {
+//   var text = document.getElementById("Text");
+
+//   var boldWords = text.getElementsByTagName("strong");
+
+//   var secilmis = "highlight";
+
+//   // Hər bir strong elementin üzərinə vurğulama üçün stil tətbiq et
+//   for (var i = 0; i < boldWords.length; i++) {
+//     boldWords[i].classList.add(secilmis);
+//   }
+// }
